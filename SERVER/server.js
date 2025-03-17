16bd92453a6cda807e6f9239703bd7dc126036ca
killlercam <herosviral@gmail.com>
2025-03-17 16:36:27 +0530

    Done base set up for node js and express and started the server

---
+++ SERVER/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const app = require("./app");
+
+const PORT=process.env.PORT_NUMBER || 8000 ;
+
+app.listen(PORT,() => {
+  console.log(`Server started at port ${PORT}`);
+});
