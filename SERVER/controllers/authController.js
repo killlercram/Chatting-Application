75e17aca9f3c4416d3a55a455427791a4488e16b
killlercam <herosviral@gmail.com>
2025-03-17 23:52:00 +0530

    created the login route which checks user and creates token for later verification

--- SERVER/controllers/authController.js
+++ SERVER/controllers/authController.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //Sign up request means creating Account
 router.post("/signup", async (req, res) => {
@@ -35,6 +37,47 @@ router.post("/signup", async (req, res) => {
       success: false,
     });
   }
+});
+
+//Login or signin for checking if user is present
+router.post("/login", async(req, res) => {
+  const {email, password} = req.body; 
+  try {
+    //1. Check if user exists or not
+   const user = await User.findOne({email: email});
+   if (!user) {
+    return res.send({
+      message: "User does not exist",
+      success: false
+    });
+   } 
+    //2. check if the password is correct
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.send({
+        message: "Incorrect Password!",
+        success: false
+      });
+     } 
+
+    //3. If the user exists and password is correct, assign a JWT
+    
+    //authentication token
+    const token = jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn: "1d"});
+
+    res.send({
+      message: "Logged-in successful",
+      success: true,
+      token: token
+    });
+
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false
+    });
+  }
+
 });
 
 module.exports = router;
