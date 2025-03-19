a34f71d53ea15f3a531fcae1dc78ef9b164a597a
killlercam <herosviral@gmail.com>
2025-03-19 19:57:49 +0530

    Created route for getting all the messages and creating new messages and also learned about params and body when to use it

--- SERVER/app.js
+++ SERVER/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const authRouter = require("./controllers/authController");
 const userRouter = require("./controllers/userController");
 const chatRouter = require("./controllers/chatController");
+const messageRouter = require("./controllers/messageController");
 
 
 const express = require("express");
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/chat",chatRouter);
+app.use("/api/message",messageRouter);
 
 
 
