dac9209ef4b8c81d41cf75e6241966298ebdbdf1
killlercam <herosviral@gmail.com>
2025-03-20 09:50:43 +0530

    Create the signup page

--- CLIENT/src/pages/Login.jsx
+++ CLIENT/src/pages/Login.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Login = () => {
+  
+  //getting and Setting the user details
+  const [user, setUser] = useState({
+    email:" ",
+    password: " "
+  });
+
+
+
+
   return (
-    <div>Login</div>
+    <div className="container">
+    <div className="container-back-img"></div>
+    <div className="container-back-color"></div>
+    <div className="card">
+    <div className="card_title">
+        <h1>Login Here</h1>
+    </div>
+    <div className="form">
+    <form onSubmit={ onFormSubmit }>
+        <input type="email" placeholder="Email" 
+        value={user.email}
+        onChange={ (e) => setUser({...user, email: e.target.value})} />
+
+        <input type="password" placeholder="Password"
+        value={user.password}
+        onChange={ (e) => setUser({...user, password: e.target.value})} />
+        <button>Login</button>
+    </form>
+    </div>
+    <div className="card_terms"> 
+        <span>Don't have an account yet?
+            <Link to="/signup">Signup Here</Link>
+        </span>
+    </div>
+    </div>
+</div>
   )
 }
 
--- CLIENT/src/pages/Signup.jsx
+++ CLIENT/src/pages/Signup.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
 const Signup = () => {
+  //values must be same as in backend schema
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  //assigning entered values above
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUser (values => ({...values, [name]: value}));
+  }
+
+  //handling Form submission
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    console.log(user);
+  }
+
   return (
     <div className="container">
+      <div className="heading">
+        <span>Quick Chat</span>
+      </div>
       <div className="container-back-img"></div>
       <div className="container-back-color"></div>
       <div className="card">
@@ -10,13 +34,37 @@ const Signup = () => {
           <h1>Create Account</h1>
         </div>
         <div className="form">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="column">
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
+              <input
+                type="text"
+                placeholder="First Name"
+                name="firstname"
+                value={user.firstname}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                name="lastname"
+                value={user.lastname}
+                onChange={handleChange}
+              />
             </div>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+            />
             <button>Sign Up</button>
           </form>
         </div>
