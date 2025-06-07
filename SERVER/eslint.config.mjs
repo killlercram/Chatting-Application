5439efda0ac7cbe153603cbd3ec43f83e63e3bb7
killlercam <herosviral@gmail.com>
2025-06-07 22:07:51 +0530

    Added linting to server

--- SERVER/eslint.config.mjs
+++ SERVER/eslint.config.mjs
@@ -1,11 +1,35 @@
+// import js from "@eslint/js";
+// import globals from "globals";
+// import pluginReact from "eslint-plugin-react";
+// import { defineConfig } from "eslint/config";
+
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+//   pluginReact.configs.flat.recommended,
+// ]);
+
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+export default defineConfig({
+  files: ["SERVER/**/*.{js,mjs,cjs}"],
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",  // or "script" if you use CommonJS fully
+    globals: {
+      ...globals.node,  // provides 'require', 'module', 'process', etc.
+    },
+  },
+
+  plugins: {
+    js,
+  },
+
+  rules: {
+    ...js.configs.recommended.rules,
+  },
+});
